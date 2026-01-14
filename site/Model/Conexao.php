6f711755af2a89387daadc759a5ea16130b92b8e
Wictor Morais Gonçalves <wictor.morais2@gmail.com>
2026-01-14 14:29:51 -0300

    Removendo dados sigilosos

--- site/Model/Conexao.php
+++ site/Model/Conexao.php
@@ -1,11 +1,11 @@
 <?php
 
 // Configurações do banco de dados remoto
-$host     = "br910.hostgator.com.br";
+$host     = "SEU_HOST_AQUI";
 // $host     = "162.241.2.71";
-$usuario  = "quaiat07_marcenaria_morais";
-$senha    = "21314151**mm";
-$banco    = "quaiat07_marcenaria_morais";
+$usuario  = "SEU_USER_AQUI";
+$senha    = "SUA_SENHA_AQUI";
+$banco    = "SEU_BANCO_AQUI";
 
 // Criar conexão usando mysqli
 $conn = new mysqli($host, $usuario, $senha, $banco);
